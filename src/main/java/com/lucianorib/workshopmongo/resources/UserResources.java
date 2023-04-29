88d56ff6b32da4ea414ad2563ce55a5c89ffb090
lucianoRib5 <luciano.eng.soft@outlook.com>
2023-04-29 00:05:21 -0300

    feat: findById and ExceptionHandler

--- src/main/java/com/lucianorib/workshopmongo/resources/UserResources.java
+++ src/main/java/com/lucianorib/workshopmongo/resources/UserResources.java
@@ -5,6 +5,7 @@ import com.lucianorib.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -20,5 +21,11 @@ public class UserResources {
         List<User> userList = userService.findAll();
         List<UserDTO> userDTOList = userList.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
