98585696b466e99ac7a801b67152ee5ac5e94e45
lucianoRib5 <luciano.eng.soft@outlook.com>
2023-04-30 23:07:02 -0300

    feat: add endpoint insertUser

--- src/main/java/com/lucianorib/workshopmongo/services/UserService.java
+++ src/main/java/com/lucianorib/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucianorib.workshopmongo.services;
 
 import com.lucianorib.workshopmongo.domain.User;
+import com.lucianorib.workshopmongo.dto.UserDTO;
 import com.lucianorib.workshopmongo.repository.UserRepository;
 import com.lucianorib.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+    }
+
+    public User insert(User user){
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
