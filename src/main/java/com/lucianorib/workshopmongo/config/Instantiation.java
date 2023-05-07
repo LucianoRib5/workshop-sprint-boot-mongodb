00059c781a644588c3f6b8fe621eb1d65d14d9c0
lucianoRib5 <luciano.eng.soft@outlook.com>
2023-05-07 20:14:57 -0300

    feat: adiciona comentários aos posts

--- src/main/java/com/lucianorib/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucianorib/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.lucianorib.workshopmongo.config;
 import com.lucianorib.workshopmongo.domain.Post;
 import com.lucianorib.workshopmongo.domain.User;
 import com.lucianorib.workshopmongo.dto.AuthorDTO;
+import com.lucianorib.workshopmongo.dto.CommentDTO;
 import com.lucianorib.workshopmongo.repository.PostRepository;
 import com.lucianorib.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,14 @@ public class Instantiation implements CommandLineRunner {
         Post post0 = new Post(null,sdf.parse("06/05/2023"),"Learn Java","Ficando brabo no Java",new AuthorDTO(luciano));
         Post post1 = new Post(null,sdf.parse("06/05/2023"),"Bikee","Aprendi a andar de bikeee!",new AuthorDTO(sebastian));
         Post post2 = new Post(null,sdf.parse("07/05/2023"),"Learning...","Estudando no sabadão",new AuthorDTO(luciano));
+
+        CommentDTO comment0 = new CommentDTO("Boaa papai, voaa!", sdf.parse("07/05/2023"), new AuthorDTO(sebastian));
+        CommentDTO comment1 = new CommentDTO("Boa amor", sdf.parse("07/05/2023"), new AuthorDTO(edwirges));
+        CommentDTO comment2 = new CommentDTO("Boa filhãoo", sdf.parse("07/05/2023"), new AuthorDTO(edwirges));
+        CommentDTO comment3 = new CommentDTO("Ai sim moleque, você é demais", sdf.parse("07/05/2023"), new AuthorDTO(luciano));
+
+        post0.getComments().addAll(Arrays.asList(comment0, comment1));
+        post1.getComments().addAll(Arrays.asList(comment2, comment3));
 
         postRepository.saveAll(Arrays.asList(post0, post1, post2));
 
--- src/main/java/com/lucianorib/workshopmongo/domain/Post.java
+++ src/main/java/com/lucianorib/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.lucianorib.workshopmongo.domain;
 
 import com.lucianorib.workshopmongo.dto.AuthorDTO;
+import com.lucianorib.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -15,6 +18,8 @@ public class Post {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -66,6 +71,14 @@ public class Post {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
